b3a5fb1c8bc64f6c9872a2482ced6a468ce3b920
PCIUKAS\Erika <erika.virbickene@gmail.com>
2022-05-12 20:21:11 +0300

    Galutinis

--- VCS2022_Baigiamasis/Page/SafloraPrekiuKrepselioIstustinimasPage.cs
+++ VCS2022_Baigiamasis/Page/SafloraPrekiuKrepselioIstustinimasPage.cs
@@ -16,8 +16,8 @@ namespace VCS2022_Baigiamasis.Page
         private const string PageAddrress = "https://www.saflora.lt/";
 
         private IWebElement _hidrolataiButton => Driver.FindElement(By.Id("menu-item-439"));
-        private IWebElement _preke => Driver.FindElement(By.CssSelector("#page > ul > li.col4.product.post-608.product.type-product.status-publish.has-post-thumbnail.product_cat-brandziai-odai.product_cat-hidrolatai.product_cat-sausai-odai.instock.purchasable.product-type-simple > div.product_in.fade_woo > a.woocommerce-LoopProduct-link.woocommerce-loop-product__link"));
-        private IWebElement _prekeIKrepseli => Driver.FindElement(By.CssSelector("#product-610 > div.summary.entry-summary > div.cart_add > form > button"));
+        private IWebElement _preke => Driver.FindElement(By.CssSelector("#page > ul > li.col4.product.post-615.product.type-product.status-publish.has-post-thumbnail.product_cat-hidrolatai.product_cat-misriai-odai.product_cat-probleminei-odai.product_cat-riebiai-ir-normaliai-odai.first.instock.purchasable.product-type-simple > div.product_in.fade_woo > a.woocommerce-LoopProduct-link.woocommerce-loop-product__link"));
+        private IWebElement _prekeIKrepseli => Driver.FindElement(By.CssSelector("#product-615 > div.summary.entry-summary > div.cart_add > form > button"));
 
         private IWebElement _krepselisButton => Driver.FindElement(By.CssSelector("#bg > div.bg_head > div.head_top_container > div.wrapper_p.top_header > div.top_con > div.toph_r > ul > li > div > div.cart_top > a"));
 
